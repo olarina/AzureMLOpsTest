b5c6572d9cf9c98be3c9009b043e355778c8ce83
Olga Larina <olarinav@gmail.com>
2022-12-27 13:23:08 +0100

    Lint fix 2

--- src/model/train.py
+++ src/model/train.py
@@ -12,7 +12,8 @@ import mlflow
 
 # define functions
 def main(args):
-    
+
+
     # read data
     df = get_csvs_df(args.training_data)
 
@@ -34,8 +35,9 @@ def get_csvs_df(path):
 
 # TO DO: add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', 'SerumInsulin',
-               'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure',
+               'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree',
+               'Age']].values, df['Diabetic'].values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30,
                                                         random_state=0)
     return X_train, X_test, y_train, y_test
@@ -65,6 +67,7 @@ def parse_args():
 
 # run script
 if __name__ == "__main__":
+
 
     # add space in logs
     print("\n\n")
